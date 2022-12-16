041bcb3a5d4c2a98e484f8b2b1891d22e05cb78c
Celia Melendi Lavandera <uo276077@uniovi.es>
2022-12-16 20:49:18 +0100

    update

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/:date', (req, res) => {
 
 // Get all logs
 router.get("/logs", async (req, res) => {
-  const { results: logsMetadata } = await logsCollection.list();
+  /*const { results: logsMetadata } = await logsCollection.list();
 
   const logs = await Promise.all(
       logsMetadata.map(async ({ key }) => (await logsCollection.get(key)).props)
@@ -60,7 +60,18 @@ router.get("/logs", async (req, res) => {
   let fileList = logs.filter(file => file.endsWith('.log'));
   let names = fileList.map(file => file.substring(0, file.length - 4));
 
-  res.render('logs.twig', {files: names});
+  res.render('logs.twig', {files: names});*/
+  fs.readdir(__dirname + '/Logs', (err, files) => {
+    if (err)
+    console.log(err);
+    else {
+      console.log("\nCurrent directory filenames:");
+      let fileList = files.filter(file => file.endsWith('.log'));
+      let names = fileList.map(file => file.substring(0, file.length - 4));
+      
+      res.render('logs.twig', {files: names});
+    }      
+  })
 });
 
 router.get('/:date', async (req, res) => {
